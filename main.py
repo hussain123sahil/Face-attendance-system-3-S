36552dd4eb72fa5eab9960e711556e44ee7dadc1
sahil <sahilhussain1231@gmail.com>
2024-04-14 21:01:38 +0530

    added all buttons and completed half frontend

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Face_Recognition_System:
         self.photoimg1=ImageTk.PhotoImage(img1)
 
         f_lbl=Label(self.root,image=self.photoimg1)
-        f_lbl.place(x=500,y=0,width=500,height=130)
+        f_lbl.place(x=525,y=0,width=500,height=130)
 
 
         #third image
@@ -44,9 +44,96 @@ class Face_Recognition_System:
         bg_img=Label(self.root,image=self.photoimg3)
         bg_img.place(x=0,y=130,width=1530,height=710)
 
+        title_lbl=Label(bg_img,text="Face Recognition Attendance System Software", font=("times new roman",35,"bold"),bg="white",fg="red")
+        title_lbl.place(x=0,y=0,width=1530,height=45)
 
+        # student button
+        img4=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img4=img4.resize((220,220),Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
 
+        b1=Button(bg_img,image=self.photoimg4,cursor="hand2")
+        b1.place(x=200,y=100,width=220,height=220)
 
+        b1_1=Button(bg_img,text="Student Details",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=300,width=220,height=40)
+
+        # face detect button
+        img5=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img5=img5.resize((220,220),Image.ANTIALIAS)
+        self.photoimg5=ImageTk.PhotoImage(img5)
+
+        b1=Button(bg_img,image=self.photoimg5,cursor="hand2")
+        b1.place(x=500,y=100,width=220,height=220)
+
+        b1_1=Button(bg_img,text="Face Detector",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=300,width=220,height=40)
+
+         # Attendance button
+        img6=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img6=img6.resize((220,220),Image.ANTIALIAS)
+        self.photoimg6=ImageTk.PhotoImage(img6)
+
+        b1=Button(bg_img,image=self.photoimg6,cursor="hand2")
+        b1.place(x=800,y=100,width=220,height=220)
+
+        b1_1=Button(bg_img,text="Attendance",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=300,width=220,height=40)
+
+         # Help button
+        img7=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img7=img7.resize((220,220),Image.ANTIALIAS)
+        self.photoimg7=ImageTk.PhotoImage(img7)
+
+        b1=Button(bg_img,image=self.photoimg7,cursor="hand2")
+        b1.place(x=1100,y=100,width=220,height=220)
+
+        b1_1=Button(bg_img,text="Help Desk",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=300,width=220,height=40)
+
+        # train face button
+        img8=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img8=img8.resize((220,220),Image.ANTIALIAS)
+        self.photoimg8=ImageTk.PhotoImage(img8)
+
+        b1=Button(bg_img,image=self.photoimg8,cursor="hand2")
+        b1.place(x=200,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="Train Data",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=580,width=220,height=40)
+
+        # photos button
+        img9=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img9=img9.resize((220,220),Image.ANTIALIAS)
+        self.photoimg9=ImageTk.PhotoImage(img9)
+
+        b1=Button(bg_img,image=self.photoimg9,cursor="hand2")
+        b1.place(x=500,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="Photos",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=580,width=220,height=40)
+
+        # developer button
+        img10=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img10=img10.resize((220,220),Image.ANTIALIAS)
+        self.photoimg10=ImageTk.PhotoImage(img10)
+
+        b1=Button(bg_img,image=self.photoimg10,cursor="hand2")
+        b1.place(x=800,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="Developers",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=580,width=220,height=40)
+
+        # exit button
+        img11=Image.open(r"E:\DBMS project\Face-attendance-system-3-S\college_images\student_icon.png")
+        img11=img11.resize((220,220),Image.ANTIALIAS)
+        self.photoimg11=ImageTk.PhotoImage(img11)
+
+        b1=Button(bg_img,image=self.photoimg11,cursor="hand2")
+        b1.place(x=1100,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="Exit",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=580,width=220,height=40)
 
 if __name__ == "__main__":
     root=Tk()
